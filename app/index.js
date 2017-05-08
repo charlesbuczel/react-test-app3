94ae51ce073f9780b75b06256f52b922c319fa53
Charles Buczel <charles.buczel@gmail.com>
2017-05-08 23:35:13 +0100

    Update

--- app/index.js
+++ app/index.js
@@ -2,17 +2,27 @@ var React = require('react');
 var ReactDom = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
+class Badge extends React.Component {
   render () {
     return(
       <div>
-        Hello Worlds!
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        ></img>
+        <h1>Name: {this.props.name}</h1>>
+        <h3>username: {this.props.username}</h3>
       </div>
     );
   }
 }
 
 ReactDom.render(
-  <App />,
-  document.getElementById('app')
+  <Badge
+    name='Charles'
+    username='charlesbuczel'
+    img='https://avatars0.githubusercontent.com/u/950505'
+  />,
+  document.getElementById('root')
 );
