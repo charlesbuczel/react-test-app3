58a72fe5eb1b9f7c84c4222e6d08e49f9fc97ee5
Charles Buczel <charles.buczel@gmail.com>
2017-05-09 23:41:59 +0100

    First comp

--- app/index.js
+++ app/index.js
@@ -1,34 +1,9 @@
 const React = require('react');
 const ReactDom = require('react-dom');
-const PropTypes = require('prop-types');
-
-class Badge extends React.Component {
-  render () {
-    return(
-      <div>
-        <img
-          src={this.props.img}
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        ></img>
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    );
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-}
+require('./index.css');
+const App = require('./components/App');
 
 ReactDom.render(
-  <Badge
-    name='Charles'
-    username='charlesbuczel'
-    img='https://avatars0.githubusercontent.com/u/950505'
-  />,
-  document.getElementById('root')
+  <App />,
+  document.getElementById('app')
 );
